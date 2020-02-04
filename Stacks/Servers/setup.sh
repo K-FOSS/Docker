17678a04654670b2cf948a3d4a69a345c4962528
root <me@kristianjones.dev>
2020-02-03 20:35:27 -0600

    feat: Testing neutred script

--- Stacks/Servers/setup.sh
+++ Stacks/Servers/setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-REQUIRED_PKGS="gnupg-agent software-properties-common socat"
+REQUIRED_PKGS="gnupg-agent apt-transport-https ca-certificates software-properties-common socat"
 
 # check os
 OSTYPE=unknown
@@ -12,7 +12,7 @@ installPackage() {
 
   # TODO: Add support for more distributions
   if [ "${OSTYPE}" == "ubuntu" ]; then
-    apt-get install --assume-yes "${packages}"
+    apt-get install --assume-yes ${packages}
   else
     echo "Installing not supported on this distribution... Yet!"
   fi
@@ -24,7 +24,7 @@ addRepo() {
 
   # TODO: Add support for more distributions
   if [ "${OSTYPE}" == "ubuntu" ]; then
-    wget -O - ${keyURL} | apt-key add -
+    wget -O - ${repoKeyURL} | apt-key add -
     add-apt-repository "deb [arch=amd64] ${repoURL} $(lsb_release -cs) stable"
     apt-get update --assume-yes
   else
@@ -36,26 +36,21 @@ echo "Installing Packages"
 
 installPackage "${REQUIRED_PKGS}"
 
-echo "Adding Docker repo keys & repo"
-
-## TODO: Move this to OS independant
 addRepo "https://download.docker.com/linux/ubuntu/gpg" "https://download.docker.com/linux/ubuntu"
-
-echo "Installing docker packages"
 
 installPackage "docker-ce docker-ce-cli containerd.io"
 
-echo "Packages installed. testing 'docker --help'."
-docker --help
+# echo "Packages installed. testing 'docker --help'."
+# docker --help
 
-echo "Installing 'docker-compose'"
+# echo "Installing 'docker-compose'"
 
-wget -O /usr/local/bin/docker-compose "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)"
+# wget -O /usr/local/bin/docker-compose "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)"
 
-echo "Making 'docker-compose.yml' executable"
+# echo "Making 'docker-compose.yml' executable"
 
-chmod +x /usr/local/bin/docker-compose
+# chmod +x /usr/local/bin/docker-compose
 
-echo "Installing Server Services Stack from the 'installServices.sh' script in the K-FOSS/Docker Stacks/Servers/installServices.sh"
+# echo "Installing Server Services Stack from the 'installServices.sh' script in the K-FOSS/Docker Stacks/Servers/installServices.sh"
 
-wget -O - https://raw.githubusercontent.com/K-FOSS/Docker/master/Stacks/Servers/installServices.sh | bash
+# wget -O - https://raw.githubusercontent.com/K-FOSS/Docker/master/Stacks/Servers/installServices.sh | bash
